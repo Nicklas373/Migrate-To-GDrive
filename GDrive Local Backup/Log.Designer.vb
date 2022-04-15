2d92badb6f3fcaffdd668caea7ebed239931250c
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2022-04-15 09:54:30 +0700

    Log: Cosmetic changes
    * Cosmetic changes on logging notifications

--- GDrive Local Backup/Log.Designer.vb
+++ GDrive Local Backup/Log.Designer.vb
@@ -39,54 +39,61 @@ Partial Class Log
         'GroupBox1
         '
         Me.GroupBox1.Controls.Add(Me.RichTextBox1)
-        Me.GroupBox1.Location = New System.Drawing.Point(108, 12)
+        Me.GroupBox1.Location = New System.Drawing.Point(123, 16)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(820, 473)
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.GroupBox1.Size = New System.Drawing.Size(937, 631)
         Me.GroupBox1.TabIndex = 0
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "Log"
         '
         'RichTextBox1
         '
-        Me.RichTextBox1.Location = New System.Drawing.Point(6, 22)
+        Me.RichTextBox1.Location = New System.Drawing.Point(7, 29)
+        Me.RichTextBox1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.RichTextBox1.Name = "RichTextBox1"
         Me.RichTextBox1.ReadOnly = True
-        Me.RichTextBox1.Size = New System.Drawing.Size(808, 445)
+        Me.RichTextBox1.Size = New System.Drawing.Size(923, 592)
         Me.RichTextBox1.TabIndex = 0
         Me.RichTextBox1.Text = "Log will show here ..."
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(6, 22)
+        Me.Button1.Location = New System.Drawing.Point(7, 29)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(90, 42)
+        Me.Button1.Size = New System.Drawing.Size(103, 56)
         Me.Button1.TabIndex = 1
         Me.Button1.Text = "Backup Log"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(6, 70)
+        Me.Button2.Location = New System.Drawing.Point(7, 93)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(90, 42)
+        Me.Button2.Size = New System.Drawing.Size(103, 56)
         Me.Button2.TabIndex = 2
         Me.Button2.Text = "Error Log"
         Me.Button2.UseVisualStyleBackColor = True
         '
         'Button3
         '
-        Me.Button3.Location = New System.Drawing.Point(6, 22)
+        Me.Button3.Location = New System.Drawing.Point(7, 29)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(90, 42)
+        Me.Button3.Size = New System.Drawing.Size(103, 56)
         Me.Button3.TabIndex = 3
         Me.Button3.Text = "Clear Log"
         Me.Button3.UseVisualStyleBackColor = True
         '
         'Button4
         '
-        Me.Button4.Location = New System.Drawing.Point(6, 70)
+        Me.Button4.Location = New System.Drawing.Point(7, 93)
+        Me.Button4.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Button4.Name = "Button4"
-        Me.Button4.Size = New System.Drawing.Size(90, 42)
+        Me.Button4.Size = New System.Drawing.Size(103, 56)
         Me.Button4.TabIndex = 4
         Me.Button4.Text = "Export Log"
         Me.Button4.UseVisualStyleBackColor = True
@@ -95,9 +102,11 @@ Partial Class Log
         '
         Me.GroupBox2.Controls.Add(Me.Button1)
         Me.GroupBox2.Controls.Add(Me.Button2)
-        Me.GroupBox2.Location = New System.Drawing.Point(3, 12)
+        Me.GroupBox2.Location = New System.Drawing.Point(3, 16)
+        Me.GroupBox2.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.GroupBox2.Name = "GroupBox2"
-        Me.GroupBox2.Size = New System.Drawing.Size(105, 124)
+        Me.GroupBox2.Padding = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.GroupBox2.Size = New System.Drawing.Size(120, 165)
         Me.GroupBox2.TabIndex = 5
         Me.GroupBox2.TabStop = False
         Me.GroupBox2.Text = "Select Log"
@@ -106,26 +115,29 @@ Partial Class Log
         '
         Me.GroupBox3.Controls.Add(Me.Button3)
         Me.GroupBox3.Controls.Add(Me.Button4)
-        Me.GroupBox3.Location = New System.Drawing.Point(3, 142)
+        Me.GroupBox3.Location = New System.Drawing.Point(3, 189)
+        Me.GroupBox3.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.GroupBox3.Name = "GroupBox3"
-        Me.GroupBox3.Size = New System.Drawing.Size(105, 124)
+        Me.GroupBox3.Padding = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.GroupBox3.Size = New System.Drawing.Size(120, 165)
         Me.GroupBox3.TabIndex = 1
         Me.GroupBox3.TabStop = False
         Me.GroupBox3.Text = "Other"
         '
         'Log
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.AutoSize = True
         Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
-        Me.ClientSize = New System.Drawing.Size(940, 497)
+        Me.ClientSize = New System.Drawing.Size(1074, 663)
         Me.Controls.Add(Me.GroupBox3)
         Me.Controls.Add(Me.GroupBox2)
         Me.Controls.Add(Me.GroupBox1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
         Me.HelpButton = True
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Name = "Log"
         Me.Text = "MigrateToGDrive | Log Menu"
         Me.GroupBox1.ResumeLayout(False)
--- GDrive Local Backup/Log.vb
+++ GDrive Local Backup/Log.vb
@@ -3,36 +3,36 @@
     Dim errPath As String = "log/err"
     Dim confPath As String = "conf/config"
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        showLog("Backup Log", logPath)
+        showLog("Backup log", logPath)
     End Sub
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        showLog("Error Log", errPath)
+        showLog("Error log", errPath)
     End Sub
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
         Dim validation As Integer
-        validation = MsgBox("This Will Remove All Log File !, Remove Log ?", vbExclamation + vbYesNo + vbDefaultButton2, "Clear Log")
+        validation = MsgBox("This will remove all log file !", vbExclamation + vbYesNo + vbDefaultButton2, "MigrateToGDrive")
         If validation = vbYes Then
-            clearLog(logPath, "Backup Log")
-            clearLog(errPath, "Error Log")
+            clearLog(logPath, "Backup log")
+            clearLog(errPath, "Error log")
         Else
-            MsgBox("Cancel Remove Log !", MsgBoxStyle.Information)
+            MsgBox("Cancel remove log !", MsgBoxStyle.Information, "MigrateToGDrive")
         End If
     End Sub
     Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
         If File.Exists(logPath) Then
             File.Copy(logPath, Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) & "/log.txt")
             If File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) & "/log.txt") Then
-                MsgBox("Export Backup Log Success !", MsgBoxStyle.Information)
+                MsgBox("Export backup log success !", MsgBoxStyle.Information, "MigrateToGDrive")
             Else
-                MsgBox("Export Backup Log Failed !", MsgBoxStyle.Critical)
+                MsgBox("Export backup log failed !", MsgBoxStyle.Critical, "MigrateToGDrive")
             End If
         End If
         If File.Exists(errPath) Then
             File.Copy(errPath, Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) & "/err.txt")
             If File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) & "/err.txt") Then
-                MsgBox("Export Error Log Success !", MsgBoxStyle.Information)
+                MsgBox("Export error log success !", MsgBoxStyle.Information, "MigrateToGDrive")
             Else
-                MsgBox("Export Error Log Failed !", MsgBoxStyle.Critical)
+                MsgBox("Export error log failed !", MsgBoxStyle.Critical, "MigrateToGDrive")
             End If
         End If
     End Sub
@@ -40,22 +40,22 @@
         RichTextBox1.Text = ""
         If File.Exists(path) Then
             If New FileInfo(path).Length.Equals(0) Then
-                MsgBox(log & " File Is Empty !", MsgBoxStyle.Critical)
+                MsgBox(log & " file is empty !", MsgBoxStyle.Critical, "MigrateToGDrive")
             Else
                 RichTextBox1.Text = File.ReadAllText(path)
             End If
         Else
-            MsgBox(log & " File Does Not Exist !", MsgBoxStyle.Critical)
+            MsgBox(log & " file does not exist !", MsgBoxStyle.Critical, "MigrateToGDrive")
         End If
     End Sub
     Private Sub clearLog(log As String, log2 As String)
         If File.Exists(log) Then
             File.Delete(log)
             File.Create(log).Dispose()
-            MsgBox(log2 & " File Cleared !", MsgBoxStyle.Information)
+            MsgBox(log2 & " file cleared !", MsgBoxStyle.Information, "MigrateToGDrive")
             RichTextBox1.Text = ""
         Else
-            MsgBox(log2 & " File Is Not Exist !", MsgBoxStyle.Critical)
+            MsgBox(log2 & " file is not exist !", MsgBoxStyle.Critical, "MigrateToGDrive")
             RichTextBox1.Text = ""
         End If
     End Sub
