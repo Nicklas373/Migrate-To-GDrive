850c34047fe46b069743876bd5d4d393005f27a4
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2022-04-19 14:31:37 +0700

    log: Re-factoring into sub-module

--- MigrateToGDrive/Log.Designer.vb
+++ MigrateToGDrive/Log.Designer.vb
@@ -30,18 +30,24 @@ Partial Class Log
         Me.Button3 = New System.Windows.Forms.Button()
         Me.Button4 = New System.Windows.Forms.Button()
         Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Button5 = New System.Windows.Forms.Button()
         Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.GroupBox4 = New System.Windows.Forms.GroupBox()
         Me.GroupBox1.SuspendLayout()
         Me.GroupBox2.SuspendLayout()
         Me.GroupBox3.SuspendLayout()
+        Me.GroupBox4.SuspendLayout()
         Me.SuspendLayout()
         '
         'GroupBox1
         '
         Me.GroupBox1.Controls.Add(Me.RichTextBox1)
-        Me.GroupBox1.Location = New System.Drawing.Point(163, 12)
+        Me.GroupBox1.Location = New System.Drawing.Point(186, 16)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(765, 473)
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.GroupBox1.Size = New System.Drawing.Size(874, 631)
         Me.GroupBox1.TabIndex = 0
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "History"
@@ -49,10 +55,11 @@ Partial Class Log
         'RichTextBox1
         '
         Me.RichTextBox1.BackColor = System.Drawing.Color.WhiteSmoke
-        Me.RichTextBox1.Location = New System.Drawing.Point(6, 22)
+        Me.RichTextBox1.Location = New System.Drawing.Point(7, 29)
+        Me.RichTextBox1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.RichTextBox1.Name = "RichTextBox1"
         Me.RichTextBox1.ReadOnly = True
-        Me.RichTextBox1.Size = New System.Drawing.Size(733, 445)
+        Me.RichTextBox1.Size = New System.Drawing.Size(837, 592)
         Me.RichTextBox1.TabIndex = 0
         Me.RichTextBox1.Text = "Backup history will show here..."
         '
@@ -61,9 +68,10 @@ Partial Class Log
         Me.Button1.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
         Me.Button1.Image = CType(resources.GetObject("Button1.Image"), System.Drawing.Image)
         Me.Button1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight
-        Me.Button1.Location = New System.Drawing.Point(6, 22)
+        Me.Button1.Location = New System.Drawing.Point(7, 29)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(142, 42)
+        Me.Button1.Size = New System.Drawing.Size(162, 56)
         Me.Button1.TabIndex = 1
         Me.Button1.Text = "Backup History"
         Me.Button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
@@ -74,11 +82,12 @@ Partial Class Log
         Me.Button2.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
         Me.Button2.Image = CType(resources.GetObject("Button2.Image"), System.Drawing.Image)
         Me.Button2.ImageAlign = System.Drawing.ContentAlignment.MiddleRight
-        Me.Button2.Location = New System.Drawing.Point(6, 70)
+        Me.Button2.Location = New System.Drawing.Point(6, 27)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(142, 42)
+        Me.Button2.Size = New System.Drawing.Size(162, 56)
         Me.Button2.TabIndex = 2
-        Me.Button2.Text = "Error History"
+        Me.Button2.Text = "Backup Error"
         Me.Button2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
         Me.Button2.UseVisualStyleBackColor = True
         '
@@ -87,9 +96,10 @@ Partial Class Log
         Me.Button3.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
         Me.Button3.Image = CType(resources.GetObject("Button3.Image"), System.Drawing.Image)
         Me.Button3.ImageAlign = System.Drawing.ContentAlignment.MiddleRight
-        Me.Button3.Location = New System.Drawing.Point(6, 22)
+        Me.Button3.Location = New System.Drawing.Point(7, 29)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(142, 42)
+        Me.Button3.Size = New System.Drawing.Size(162, 56)
         Me.Button3.TabIndex = 3
         Me.Button3.Text = "Clear History"
         Me.Button3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
@@ -100,9 +110,10 @@ Partial Class Log
         Me.Button4.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
         Me.Button4.Image = CType(resources.GetObject("Button4.Image"), System.Drawing.Image)
         Me.Button4.ImageAlign = System.Drawing.ContentAlignment.MiddleRight
-        Me.Button4.Location = New System.Drawing.Point(6, 70)
+        Me.Button4.Location = New System.Drawing.Point(7, 93)
+        Me.Button4.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Button4.Name = "Button4"
-        Me.Button4.Size = New System.Drawing.Size(142, 42)
+        Me.Button4.Size = New System.Drawing.Size(162, 56)
         Me.Button4.TabIndex = 4
         Me.Button4.Text = "Export History"
         Me.Button4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
@@ -110,45 +121,90 @@ Partial Class Log
         '
         'GroupBox2
         '
+        Me.GroupBox2.Controls.Add(Me.Button5)
         Me.GroupBox2.Controls.Add(Me.Button1)
-        Me.GroupBox2.Controls.Add(Me.Button2)
-        Me.GroupBox2.Location = New System.Drawing.Point(3, 12)
+        Me.GroupBox2.Location = New System.Drawing.Point(3, 16)
+        Me.GroupBox2.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.GroupBox2.Name = "GroupBox2"
-        Me.GroupBox2.Size = New System.Drawing.Size(154, 124)
+        Me.GroupBox2.Padding = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.GroupBox2.Size = New System.Drawing.Size(176, 169)
         Me.GroupBox2.TabIndex = 5
         Me.GroupBox2.TabStop = False
-        Me.GroupBox2.Text = "Select History"
+        Me.GroupBox2.Text = "Backup History"
+        '
+        'Button5
+        '
+        Me.Button5.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button5.Image = CType(resources.GetObject("Button5.Image"), System.Drawing.Image)
+        Me.Button5.ImageAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.Button5.Location = New System.Drawing.Point(7, 93)
+        Me.Button5.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(162, 56)
+        Me.Button5.TabIndex = 3
+        Me.Button5.Text = "Archive History"
+        Me.Button5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Button5.UseVisualStyleBackColor = True
         '
         'GroupBox3
         '
         Me.GroupBox3.Controls.Add(Me.Button3)
         Me.GroupBox3.Controls.Add(Me.Button4)
-        Me.GroupBox3.Location = New System.Drawing.Point(3, 142)
+        Me.GroupBox3.Location = New System.Drawing.Point(3, 368)
+        Me.GroupBox3.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.GroupBox3.Name = "GroupBox3"
-        Me.GroupBox3.Size = New System.Drawing.Size(154, 124)
+        Me.GroupBox3.Padding = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.GroupBox3.Size = New System.Drawing.Size(176, 165)
         Me.GroupBox3.TabIndex = 1
         Me.GroupBox3.TabStop = False
         Me.GroupBox3.Text = "Tools"
         '
+        'Button6
+        '
+        Me.Button6.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button6.Image = CType(resources.GetObject("Button6.Image"), System.Drawing.Image)
+        Me.Button6.ImageAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.Button6.Location = New System.Drawing.Point(6, 91)
+        Me.Button6.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(162, 56)
+        Me.Button6.TabIndex = 4
+        Me.Button6.Text = "Archive Error"
+        Me.Button6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'GroupBox4
+        '
+        Me.GroupBox4.Controls.Add(Me.Button6)
+        Me.GroupBox4.Controls.Add(Me.Button2)
+        Me.GroupBox4.Location = New System.Drawing.Point(3, 192)
+        Me.GroupBox4.Name = "GroupBox4"
+        Me.GroupBox4.Size = New System.Drawing.Size(176, 169)
+        Me.GroupBox4.TabIndex = 6
+        Me.GroupBox4.TabStop = False
+        Me.GroupBox4.Text = "Error History"
+        '
         'Log
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.AutoSize = True
         Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
         Me.BackColor = System.Drawing.Color.LightSteelBlue
-        Me.ClientSize = New System.Drawing.Size(940, 497)
+        Me.ClientSize = New System.Drawing.Size(1074, 663)
+        Me.Controls.Add(Me.GroupBox4)
         Me.Controls.Add(Me.GroupBox3)
         Me.Controls.Add(Me.GroupBox2)
         Me.Controls.Add(Me.GroupBox1)
-        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
         Me.HelpButton = True
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Name = "Log"
         Me.Text = "MigrateToGDrive | History"
         Me.GroupBox1.ResumeLayout(False)
         Me.GroupBox2.ResumeLayout(False)
         Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox4.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
@@ -161,4 +217,7 @@ Partial Class Log
     Friend WithEvents Button4 As Button
     Friend WithEvents GroupBox2 As GroupBox
     Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents GroupBox4 As GroupBox
 End Class
--- MigrateToGDrive/Log.vb
+++ MigrateToGDrive/Log.vb
@@ -1,45 +1,42 @@
 ﻿Public Class Log
     Dim logPath As String = "log/log"
+    Dim advLogPath As String = "log/advlog"
     Dim errPath As String = "log/err"
-    Dim confPath As String = "conf/config"
+    Dim advErrPath As String = "log/adverr"
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         showLog("Backup history", logPath)
     End Sub
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-        showLog("Error history", errPath)
+        showLog("Backup error history", errPath)
+    End Sub
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        showLog("Archive history", advLogPath)
+    End Sub
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        showLog("Archive error history", advErrPath)
     End Sub
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
         Dim validation As Integer
         validation = MsgBox("This will remove all history !", vbExclamation + vbYesNo + vbDefaultButton2, "MigrateToGDrive")
         If validation = vbYes Then
             clearLog(logPath, "Backup history")
-            clearLog(errPath, "Error history")
+            clearLog(advLogPath, "Archive history")
+            clearLog(errPath, "Backup error history")
+            clearLog(advErrPath, "Archive error history")
         Else
             MsgBox("Cancel remove history !", MsgBoxStyle.Information, "MigrateToGDrive")
         End If
     End Sub
     Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
-        If File.Exists(logPath) Then
-            File.Copy(logPath, Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) & "/log.txt")
-            If File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) & "/log.txt") Then
-                MsgBox("Export backup history success !", MsgBoxStyle.Information, "MigrateToGDrive")
-            Else
-                MsgBox("Export backup history failed !", MsgBoxStyle.Critical, "MigrateToGDrive")
-            End If
-        End If
-        If File.Exists(errPath) Then
-            File.Copy(errPath, Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) & "/err.txt")
-            If File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) & "/err.txt") Then
-                MsgBox("Export error history success !", MsgBoxStyle.Information, "MigrateToGDrive")
-            Else
-                MsgBox("Export error history failed !", MsgBoxStyle.Critical, "MigrateToGDrive")
-            End If
-        End If
+        ExportLog(logPath, "log", "Backup history")
+        ExportLog(advLogPath, "advLog", "Archive history")
+        ExportLog(errPath, "err", "Backup error history")
+        ExportLog(advErrPath, "advErr", "Archive error history")
     End Sub
     Private Sub Log_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Me.AllowTransparency = False
     End Sub
-    Private Sub showLog(log As String, path As String)
+    Private Sub ShowLog(log As String, path As String)
         RichTextBox1.Text = ""
         If File.Exists(path) Then
             If New FileInfo(path).Length.Equals(0) Then
@@ -51,7 +48,7 @@
             MsgBox(log & " does not exist !", MsgBoxStyle.Critical, "MigrateToGDrive")
         End If
     End Sub
-    Private Sub clearLog(log As String, log2 As String)
+    Private Sub ClearLog(log As String, log2 As String)
         If File.Exists(log) Then
             File.Delete(log)
             File.Create(log).Dispose()
@@ -60,6 +57,16 @@
         Else
             MsgBox(log2 & " is not exist !", MsgBoxStyle.Critical, "MigrateToGDrive")
             RichTextBox1.Text = ""
+        End If
+    End Sub
+    Private Sub ExportLog(logpath As String, filename As String, logname As String)
+        If File.Exists(logpath) Then
+            File.Copy(logpath, Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) & "/" & filename & ".txt")
+            If File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) & "/" & filename & ".txt") Then
+                MsgBox("Export " & logname & " success !", MsgBoxStyle.Information, "MigrateToGDrive")
+            Else
+                MsgBox("Export " & logname & " failed !", MsgBoxStyle.Critical, "MigrateToGDrive")
+            End If
         End If
     End Sub
 End Class
