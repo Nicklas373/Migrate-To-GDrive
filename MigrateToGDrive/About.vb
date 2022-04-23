865468374ac55874d32d40f403f2bcff4e61c23a
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2022-04-23 11:26:21 +0700

    About: Update release date

--- MigrateToGDrive/About.vb
+++ MigrateToGDrive/About.vb
@@ -1,21 +1,20 @@
 ﻿Public Class About
     Dim changelog As String = "changelog.txt"
     Private Sub About_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Me.AllowTransparency = False
+        AllowTransparency = False
         TextBox1.Text = My.Application.Info.ProductName
         TextBox2.Text = My.Application.Info.Version.ToString
-        TextBox3.Text = "April, 19 2022"
+        TextBox3.Text = "April, 23 2022"
         TextBox4.Text = My.Application.Info.Copyright
         TextBox5.Text = My.Application.Info.DirectoryPath
         RichTextBox1.Text = "Description: " & vbCrLf & vbCrLf & "Migrate To GDrive is a " & My.Application.Info.Description
-        readLog("changelog", changelog)
+        ReadLog("changelog", changelog)
         TextBox6.Text = "PLACEHOLDER"
-        TextBox7.Text = My.Computer.Name.ToString
-        TextBox8.Text = My.Computer.Info.OSFullName.ToString
-        TextBox9.Text = My.Computer.Info.OSVersion.ToString
+        TextBox7.Text = Computer.Name.ToString
+        TextBox8.Text = Computer.Info.OSFullName.ToString
+        TextBox9.Text = Computer.Info.OSVersion.ToString
     End Sub
-
-    Private Sub readLog(log As String, path As String)
+    Private Sub ReadLog(log As String, path As String)
         RichTextBox2.Text = ""
         If File.Exists(path) Then
             If New FileInfo(path).Length.Equals(0) Then
