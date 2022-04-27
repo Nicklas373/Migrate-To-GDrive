951f25b7df27d0808a6557be5b664f75a6a2f734
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2022-04-27 13:30:16 +0700

    Log: Do not loop notifications when export log
    Now export log notifications only show once instead of show everytime log file was exported

--- MigrateToGDrive/Log.vb
+++ MigrateToGDrive/Log.vb
@@ -1,10 +1,12 @@
 ﻿Public Class Log
-    Dim logPath As String = "log/log"
-    Dim advLogPath As String = "log/advlog"
-    Dim resLogPath As String = "log/reslog"
-    Dim errPath As String = "log/err"
-    Dim advErrPath As String = "log/adverr"
-    Dim resErrPath As String = "log/reserr"
+    ReadOnly logPath As String = "log/log"
+    ReadOnly advLogPath As String = "log/advlog"
+    ReadOnly resLogPath As String = "log/reslog"
+    ReadOnly roboLogPath As String = "log/robolog"
+    ReadOnly errPath As String = "log/err"
+    ReadOnly advErrPath As String = "log/adverr"
+    ReadOnly resErrPath As String = "log/reserr"
+    Dim logCountPath As String = "log/expLog"
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         showLog("Backup history", logPath)
     End Sub
@@ -33,6 +35,7 @@
             ClearLog(advErrPath, "Archive error history")
             ClearLog(resLogPath, "Restore history")
             ClearLog(resErrPath, "Restore error history")
+            ClearLog(roboLogPath, "Robocopy hustory")
             If New FileInfo(logPath).Length.Equals(0) Or New FileInfo(advLogPath).Length.Equals(0) Or New FileInfo(resLogPath).Length.Equals(0) Or New FileInfo(errPath).Length.Equals(0) Or New FileInfo(advErrPath).Length.Equals(0) Or New FileInfo(resErrPath).Length.Equals(0) Then
                 MsgBox("All history cleared !", MsgBoxStyle.Information, "MigrateToGDrive")
                 RichTextBox1.Text = ""
@@ -42,12 +45,20 @@
         End If
     End Sub
     Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
-        ExportLog(logPath, "log", "Backup history")
-        ExportLog(advLogPath, "advLog", "Archive history")
-        ExportLog(errPath, "err", "Backup error history")
-        ExportLog(advErrPath, "advErr", "Archive error history")
-        ExportLog(resLogPath, "resLog", "Restore history")
-        ExportLog(resErrPath, "resErr", "Restore error history")
+        ExportLog(logPath, "log", "Backup history", logCountPath)
+        ExportLog(advLogPath, "advLog", "Archive history", logCountPath)
+        ExportLog(errPath, "err", "Backup error history", logCountPath)
+        ExportLog(advErrPath, "advErr", "Archive error history", logCountPath)
+        ExportLog(resLogPath, "resLog", "Restore history", logCountPath)
+        ExportLog(resErrPath, "resErr", "Restore error history", logCountPath)
+        ExportLog(roboLogPath, "robolog", "Robolog history", logCountPath)
+        If CInt(PathVal(logCountPath, 0)) = 7 Then
+            MsgBox("All log exported !", MsgBoxStyle.Information, "MigrateToGDrive")
+        Else
+            MsgBox("Export log error !", MsgBoxStyle.Critical, "MigrateToGDrive")
+        End If
+        PrepareNotif(logCountPath)
+        RichTextBox1.Text = ""
     End Sub
     Private Sub Log_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         AllowTransparency = False
@@ -64,20 +75,41 @@
             MsgBox(log & " does not exist !", MsgBoxStyle.Critical, "MigrateToGDrive")
         End If
     End Sub
-    Private Sub ClearLog(log As String, log2 As String)
+    Private Shared Sub ClearLog(log As String, log2 As String)
         If File.Exists(log) Then
             File.Delete(log)
             File.Create(log).Dispose()
         End If
     End Sub
-    Private Sub ExportLog(logpath As String, filename As String, logname As String)
+    Private Shared Sub ExportLog(logpath As String, filename As String, logname As String, logCountPath As String)
+        Dim logCount As Integer
+        Dim curCount As Integer
+        Dim totalCount As Integer
         If File.Exists(logpath) Then
-            File.Copy(logpath, Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) & "/" & filename & ".txt")
             If File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) & "/" & filename & ".txt") Then
-                MsgBox("Export " & logname & " success !", MsgBoxStyle.Information, "MigrateToGDrive")
+                File.Delete(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) & "/" & filename & ".txt")
+                File.Copy(logpath, Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) & "/" & filename & ".txt")
             Else
-                MsgBox("Export " & logname & " failed !", MsgBoxStyle.Critical, "MigrateToGDrive")
+                File.Copy(logpath, Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) & "/" & filename & ".txt")
             End If
+            logCount += 1
         End If
+        curCount = CheckNull(logCountPath)
+        totalCount = curCount + logCount
+        PrepareNotif(logCountPath)
+        Dim destwriter As New StreamWriter(logCountPath, True)
+        destwriter.WriteLine(totalCount)
+        destwriter.Close()
     End Sub
+
+    Private Shared Function CheckNull(curCount As String) As Integer
+        Dim result As Integer
+        If PathVal(curCount, 0).Equals("null") Then
+            result = 0
+            Return result
+        Else
+            result = CInt(PathVal(curCount, 0))
+            Return result
+        End If
+    End Function
 End Class
