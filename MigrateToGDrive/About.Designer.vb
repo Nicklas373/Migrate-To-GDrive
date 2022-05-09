0912931f2e831b4eb471663f0966430e36b2809b
Dicky Herlambang (花） <herlambangdicky5@gmail.com>
2022-05-09 10:23:31 +0700

    MigrateToGDrive: Bump to v1.1.4

--- MigrateToGDrive/About.Designer.vb
+++ MigrateToGDrive/About.Designer.vb
@@ -57,9 +57,10 @@ Partial Class About
         '
         Me.PictureBox1.BackgroundImage = CType(resources.GetObject("PictureBox1.BackgroundImage"), System.Drawing.Image)
         Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.PictureBox1.Location = New System.Drawing.Point(2, 12)
+        Me.PictureBox1.Location = New System.Drawing.Point(2, 16)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(219, 207)
+        Me.PictureBox1.Size = New System.Drawing.Size(250, 276)
         Me.PictureBox1.TabIndex = 0
         Me.PictureBox1.TabStop = False
         '
@@ -75,9 +76,11 @@ Partial Class About
         Me.GroupBox1.Controls.Add(Me.Label2)
         Me.GroupBox1.Controls.Add(Me.TextBox1)
         Me.GroupBox1.Controls.Add(Me.Label1)
-        Me.GroupBox1.Location = New System.Drawing.Point(227, 12)
+        Me.GroupBox1.Location = New System.Drawing.Point(259, 16)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(705, 207)
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.GroupBox1.Size = New System.Drawing.Size(806, 276)
         Me.GroupBox1.TabIndex = 1
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "About"
@@ -86,19 +89,20 @@ Partial Class About
         '
         Me.TextBox5.BackColor = System.Drawing.SystemColors.ButtonHighlight
         Me.TextBox5.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.TextBox5.Location = New System.Drawing.Point(191, 176)
+        Me.TextBox5.Location = New System.Drawing.Point(218, 235)
+        Me.TextBox5.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.TextBox5.Name = "TextBox5"
         Me.TextBox5.ReadOnly = True
-        Me.TextBox5.Size = New System.Drawing.Size(414, 25)
+        Me.TextBox5.Size = New System.Drawing.Size(473, 29)
         Me.TextBox5.TabIndex = 11
         '
         'Label5
         '
         Me.Label5.AutoSize = True
         Me.Label5.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.Label5.Location = New System.Drawing.Point(6, 176)
+        Me.Label5.Location = New System.Drawing.Point(7, 235)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(110, 17)
+        Me.Label5.Size = New System.Drawing.Size(140, 23)
         Me.Label5.TabIndex = 10
         Me.Label5.Text = "Installation Path"
         '
@@ -106,19 +110,20 @@ Partial Class About
         '
         Me.TextBox4.BackColor = System.Drawing.SystemColors.ButtonHighlight
         Me.TextBox4.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.TextBox4.Location = New System.Drawing.Point(191, 141)
+        Me.TextBox4.Location = New System.Drawing.Point(218, 188)
+        Me.TextBox4.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.TextBox4.Name = "TextBox4"
         Me.TextBox4.ReadOnly = True
-        Me.TextBox4.Size = New System.Drawing.Size(414, 25)
+        Me.TextBox4.Size = New System.Drawing.Size(473, 29)
         Me.TextBox4.TabIndex = 9
         '
         'Label4
         '
         Me.Label4.AutoSize = True
         Me.Label4.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.Label4.Location = New System.Drawing.Point(6, 141)
+        Me.Label4.Location = New System.Drawing.Point(7, 188)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(69, 17)
+        Me.Label4.Size = New System.Drawing.Size(91, 23)
         Me.Label4.TabIndex = 8
         Me.Label4.Text = "Copyright"
         '
@@ -126,19 +131,20 @@ Partial Class About
         '
         Me.TextBox3.BackColor = System.Drawing.SystemColors.ButtonHighlight
         Me.TextBox3.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.TextBox3.Location = New System.Drawing.Point(191, 106)
+        Me.TextBox3.Location = New System.Drawing.Point(218, 141)
+        Me.TextBox3.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.TextBox3.Name = "TextBox3"
         Me.TextBox3.ReadOnly = True
-        Me.TextBox3.Size = New System.Drawing.Size(414, 25)
+        Me.TextBox3.Size = New System.Drawing.Size(473, 29)
         Me.TextBox3.TabIndex = 7
         '
         'Label3
         '
         Me.Label3.AutoSize = True
         Me.Label3.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.Label3.Location = New System.Drawing.Point(6, 106)
+        Me.Label3.Location = New System.Drawing.Point(7, 141)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(87, 17)
+        Me.Label3.Size = New System.Drawing.Size(112, 23)
         Me.Label3.TabIndex = 6
         Me.Label3.Text = "Release Date"
         '
@@ -146,19 +152,20 @@ Partial Class About
         '
         Me.TextBox2.BackColor = System.Drawing.SystemColors.ButtonHighlight
         Me.TextBox2.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.TextBox2.Location = New System.Drawing.Point(191, 71)
+        Me.TextBox2.Location = New System.Drawing.Point(218, 95)
+        Me.TextBox2.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.TextBox2.Name = "TextBox2"
         Me.TextBox2.ReadOnly = True
-        Me.TextBox2.Size = New System.Drawing.Size(414, 25)
+        Me.TextBox2.Size = New System.Drawing.Size(473, 29)
         Me.TextBox2.TabIndex = 5
         '
         'Label2
         '
         Me.Label2.AutoSize = True
         Me.Label2.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.Label2.Location = New System.Drawing.Point(6, 71)
+        Me.Label2.Location = New System.Drawing.Point(7, 95)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(129, 17)
+        Me.Label2.Size = New System.Drawing.Size(166, 23)
         Me.Label2.TabIndex = 4
         Me.Label2.Text = "Application Version"
         '
@@ -166,19 +173,20 @@ Partial Class About
         '
         Me.TextBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight
         Me.TextBox1.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.TextBox1.Location = New System.Drawing.Point(191, 35)
+        Me.TextBox1.Location = New System.Drawing.Point(218, 47)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.TextBox1.Name = "TextBox1"
         Me.TextBox1.ReadOnly = True
-        Me.TextBox1.Size = New System.Drawing.Size(414, 25)
+        Me.TextBox1.Size = New System.Drawing.Size(473, 29)
         Me.TextBox1.TabIndex = 3
         '
         'Label1
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.Label1.Location = New System.Drawing.Point(6, 36)
+        Me.Label1.Location = New System.Drawing.Point(7, 48)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(119, 17)
+        Me.Label1.Size = New System.Drawing.Size(155, 23)
         Me.Label1.TabIndex = 2
         Me.Label1.Text = "Application Name"
         '
@@ -193,9 +201,11 @@ Partial Class About
         Me.GroupBox2.Controls.Add(Me.TextBox7)
         Me.GroupBox2.Controls.Add(Me.Label7)
         Me.GroupBox2.Controls.Add(Me.RichTextBox1)
-        Me.GroupBox2.Location = New System.Drawing.Point(2, 225)
+        Me.GroupBox2.Location = New System.Drawing.Point(2, 300)
+        Me.GroupBox2.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.GroupBox2.Name = "GroupBox2"
-        Me.GroupBox2.Size = New System.Drawing.Size(409, 264)
+        Me.GroupBox2.Padding = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.GroupBox2.Size = New System.Drawing.Size(467, 352)
         Me.GroupBox2.TabIndex = 2
         Me.GroupBox2.TabStop = False
         Me.GroupBox2.Text = "Additional Information"
@@ -204,19 +214,20 @@ Partial Class About
         '
         Me.TextBox9.BackColor = System.Drawing.SystemColors.ButtonHighlight
         Me.TextBox9.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.TextBox9.Location = New System.Drawing.Point(161, 228)
+        Me.TextBox9.Location = New System.Drawing.Point(184, 304)
+        Me.TextBox9.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.TextBox9.Name = "TextBox9"
         Me.TextBox9.ReadOnly = True
-        Me.TextBox9.Size = New System.Drawing.Size(242, 25)
+        Me.TextBox9.Size = New System.Drawing.Size(276, 29)
         Me.TextBox9.TabIndex = 20
         '
         'Label9
         '
         Me.Label9.AutoSize = True
         Me.Label9.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.Label9.Location = New System.Drawing.Point(10, 231)
+        Me.Label9.Location = New System.Drawing.Point(11, 308)
         Me.Label9.Name = "Label9"
-        Me.Label9.Size = New System.Drawing.Size(75, 17)
+        Me.Label9.Size = New System.Drawing.Size(96, 23)
         Me.Label9.TabIndex = 19
         Me.Label9.Text = "OS Version"
         '
@@ -224,19 +235,20 @@ Partial Class About
         '
         Me.TextBox8.BackColor = System.Drawing.SystemColors.ButtonHighlight
         Me.TextBox8.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.TextBox8.Location = New System.Drawing.Point(161, 197)
+        Me.TextBox8.Location = New System.Drawing.Point(184, 263)
+        Me.TextBox8.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.TextBox8.Name = "TextBox8"
         Me.TextBox8.ReadOnly = True
-        Me.TextBox8.Size = New System.Drawing.Size(242, 25)
+        Me.TextBox8.Size = New System.Drawing.Size(276, 29)
         Me.TextBox8.TabIndex = 18
         '
         'Label8
         '
         Me.Label8.AutoSize = True
         Me.Label8.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.Label8.Location = New System.Drawing.Point(10, 200)
+        Me.Label8.Location = New System.Drawing.Point(11, 267)
         Me.Label8.Name = "Label8"
-        Me.Label8.Size = New System.Drawing.Size(65, 17)
+        Me.Label8.Size = New System.Drawing.Size(85, 23)
         Me.Label8.TabIndex = 17
         Me.Label8.Text = "OS Name"
         '
@@ -244,19 +256,20 @@ Partial Class About
         '
         Me.TextBox6.BackColor = System.Drawing.SystemColors.ButtonHighlight
         Me.TextBox6.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.TextBox6.Location = New System.Drawing.Point(161, 166)
+        Me.TextBox6.Location = New System.Drawing.Point(184, 221)
+        Me.TextBox6.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.TextBox6.Name = "TextBox6"
         Me.TextBox6.ReadOnly = True
-        Me.TextBox6.Size = New System.Drawing.Size(242, 25)
+        Me.TextBox6.Size = New System.Drawing.Size(276, 29)
         Me.TextBox6.TabIndex = 16
         '
         'Label6
         '
         Me.Label6.AutoSize = True
         Me.Label6.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.Label6.Location = New System.Drawing.Point(10, 169)
+        Me.Label6.Location = New System.Drawing.Point(11, 225)
         Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(66, 17)
+        Me.Label6.Size = New System.Drawing.Size(86, 23)
         Me.Label6.TabIndex = 15
         Me.Label6.Text = "Company"
         '
@@ -264,19 +277,20 @@ Partial Class About
         '
         Me.TextBox7.BackColor = System.Drawing.SystemColors.ButtonHighlight
         Me.TextBox7.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.TextBox7.Location = New System.Drawing.Point(161, 135)
+        Me.TextBox7.Location = New System.Drawing.Point(184, 180)
+        Me.TextBox7.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.TextBox7.Name = "TextBox7"
         Me.TextBox7.ReadOnly = True
-        Me.TextBox7.Size = New System.Drawing.Size(242, 25)
+        Me.TextBox7.Size = New System.Drawing.Size(276, 29)
         Me.TextBox7.TabIndex = 14
         '
         'Label7
         '
         Me.Label7.AutoSize = True
         Me.Label7.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.Label7.Location = New System.Drawing.Point(10, 138)
+        Me.Label7.Location = New System.Drawing.Point(11, 184)
         Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(35, 17)
+        Me.Label7.Size = New System.Drawing.Size(45, 23)
         Me.Label7.TabIndex = 13
         Me.Label7.Text = "User"
         '
@@ -284,19 +298,22 @@ Partial Class About
         '
         Me.RichTextBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight
         Me.RichTextBox1.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.RichTextBox1.Location = New System.Drawing.Point(10, 22)
+        Me.RichTextBox1.Location = New System.Drawing.Point(11, 29)
+        Me.RichTextBox1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.RichTextBox1.Name = "RichTextBox1"
         Me.RichTextBox1.ReadOnly = True
-        Me.RichTextBox1.Size = New System.Drawing.Size(393, 103)
+        Me.RichTextBox1.Size = New System.Drawing.Size(449, 136)
         Me.RichTextBox1.TabIndex = 3
         Me.RichTextBox1.Text = ""
         '
         'GroupBox3
         '
         Me.GroupBox3.Controls.Add(Me.RichTextBox2)
-        Me.GroupBox3.Location = New System.Drawing.Point(418, 225)
+        Me.GroupBox3.Location = New System.Drawing.Point(478, 300)
+        Me.GroupBox3.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.GroupBox3.Name = "GroupBox3"
-        Me.GroupBox3.Size = New System.Drawing.Size(514, 264)
+        Me.GroupBox3.Padding = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.GroupBox3.Size = New System.Drawing.Size(587, 352)
         Me.GroupBox3.TabIndex = 3
         Me.GroupBox3.TabStop = False
         Me.GroupBox3.Text = "Changelog"
@@ -304,24 +321,26 @@ Partial Class About
         'RichTextBox2
         '
         Me.RichTextBox2.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
-        Me.RichTextBox2.Location = New System.Drawing.Point(6, 22)
+        Me.RichTextBox2.Location = New System.Drawing.Point(7, 29)
+        Me.RichTextBox2.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.RichTextBox2.Name = "RichTextBox2"
-        Me.RichTextBox2.Size = New System.Drawing.Size(502, 225)
+        Me.RichTextBox2.Size = New System.Drawing.Size(573, 299)
         Me.RichTextBox2.TabIndex = 0
         Me.RichTextBox2.Text = ""
         '
         'About
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.AutoScroll = True
         Me.BackColor = System.Drawing.Color.LightSteelBlue
-        Me.ClientSize = New System.Drawing.Size(944, 501)
+        Me.ClientSize = New System.Drawing.Size(1079, 668)
         Me.Controls.Add(Me.GroupBox3)
         Me.Controls.Add(Me.GroupBox2)
         Me.Controls.Add(Me.GroupBox1)
         Me.Controls.Add(Me.PictureBox1)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Name = "About"
         Me.Text = "MigrateToGDrive | About"
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
